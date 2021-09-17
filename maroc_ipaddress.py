a10a199d7ff052e741ece2f6a5c1600f795588a0
Chiara De Sio <chiara.desio@bristol.ac.uk>
2021-09-17 12:28:34 +0100

    not using pandas anymore - it's a dict of named tuple

--- maroc_ipaddress.py
+++ maroc_ipaddress.py
@@ -1,15 +1,14 @@
+from io import BufferedIOBase
 import ipaddress
 import itertools
 import numpy as np
-import pandas as pd
+from collections import namedtuple
 
 lst = list(itertools.product([0, 1], repeat=4))
-
 
 def ip_last(quad, jumper):
     a, b, c, d = quad
     return (2 ** 0 * a + 2 ** 1 * b + 2 ** 2 * c + 2 ** 3 * d) * 16 + jumper
-
 
 ipaddr = []
 ipdec = []
@@ -24,14 +23,6 @@ idx = np.argsort([int(addr.split(".")[-1]) for addr in ipaddr])
 
 sorted_ipaddr = np.asarray(ipaddr)[idx][2:]
 sorted_dec = np.asarray(ipdec)[idx][2:]
-"""
-boards_ip = {}
-for i, (ip, dec) in enumerate(zip(sorted_ipaddr, sorted_dec)):
-    boards_ip[i + 1] = {}
-    boards_ip[i + 1]["ip"] = ip
-    boards_ip[i + 1]["dec"] = dec
-    """
-boards_ip = pd.DataFrame(
-    list(zip(np.arange(1, 31), sorted_ipaddr, sorted_dec)),
-    columns=["board_id", "ip", "dec"],
-)
+
+BoardIP = namedtuple("BoardIP", ["bid", "ipv4", "dec"])
+BOARDS = {dec: BoardIP(bid, ip, dec) for bid, ip, dec in zip(range(1, 31), sorted_ipaddr, sorted_dec)}
