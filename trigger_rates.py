a8a0d1be4a2e3f2b042de74b812a207ae097bb5d
Chiara De Sio <chiara.desio@bristol.ac.uk>
2021-10-13 13:42:21 +0100

    writing filetimes to output file- going back to fnam

--- trigger_rates.py
+++ trigger_rates.py
@@ -30,12 +30,9 @@ for (i, ts), (j, tsp1) in zip(enumerate(filelist), enumerate(filelist[1:])):
     filetime2, events2 = get_max_evt_number(folder, tsp1)
     delta_ft = filetime2 - filetime1
     if delta_ft.seconds > 0 and delta_ft.seconds < 3600:
-        rates[ts] = (filetime1, events1 / delta_ft.seconds)
+        rates[ts] = events1 / delta_ft.seconds
         # print(filetime1, filetime2, ts, tsp1, delta_ft.seconds, np.ceil(events1/delta_ft.seconds))
-print(
-    np.asarray(list(rates.values()))[:, 1],
-    np.mean(np.asarray(list(rates.values()))[:, 1]),
-)
+print(rates, np.mean(rates.values()))
 
 import json
 
