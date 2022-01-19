f02aac8fa1ae4f000dd07a8831ada45e5309084b
Chiara De Sio <chiara.desio@bristol.ac.uk>
2022-01-19 12:47:43 +0000

    script to run from a directory

---
+++ plot_run_pipeline_dir.py
@@ -0,0 +1,14 @@
+import sys, os
+import numpy as np
+
+if __name__ == "__main__":
+
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    for fil in os.listdir(input_dir):
+        if fil.startswith("Run") and fil.endswith(".dat"):
+            os.system(
+                f"python ./plot_run_pipeline.py {os.path.join(input_dir, fil)} 4 3 {output_dir}"
+            )
+    print("done.")
